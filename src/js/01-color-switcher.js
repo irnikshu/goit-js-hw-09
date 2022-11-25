7be28db4b1286e03f6f222aa42ad78f9dee35701
irnikshu <95768966+irnikshu@users.noreply.github.com>
2022-11-25 19:34:38 +0200

    Task 01

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,32 @@
+const startBtn = document.querySelector('button[data-start]');
+const stopBtn = document.querySelector('button[data-stop]');
+const body = document.body;
+
+// stopBtn.disabled = true;
+
+let timerId = null;
+
+startBtn.addEventListener('click', startChangeBodyColor);
+stopBtn.addEventListener('click', stopChangeBodyColor);
+
+function startChangeBodyColor(event) {
+    timerId = setInterval(backgroundColor, 1000);
+    startBtn.disabled = true;
+    stopBtn.disabled = false;
+};
+
+function backgroundColor() {
+    body.style.backgroundColor = getRandomHexColor();
+};
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+};
+
+function stopChangeBodyColor() {
+    clearInterval(timerId);
+    startBtn.disabled = false;
+    stopBtn.disabled = true;
+};
+
 

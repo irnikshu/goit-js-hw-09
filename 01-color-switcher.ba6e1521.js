2f4539d0b4077587fcf5fa6b9401dd8d205390d8
irnikshu <95768966+irnikshu@users.noreply.github.com>
2022-11-25 17:35:36 +0000

    Deploying to gh-pages from @ irnikshu/goit-js-hw-09@7be28db4b1286e03f6f222aa42ad78f9dee35701 🚀

---
+++ 01-color-switcher.ba6e1521.js
@@ -0,0 +1,2 @@
+!function(){var t=document.querySelector("button[data-start]"),e=document.querySelector("button[data-stop]"),n=document.body,o=null;function a(){n.style.backgroundColor="#".concat(Math.floor(16777215*Math.random()).toString(16))}t.addEventListener("click",(function(n){o=setInterval(a,1e3),t.disabled=!0,e.disabled=!1})),e.addEventListener("click",(function(){clearInterval(o),t.disabled=!1,e.disabled=!0}))}();
+//# sourceMappingURL=01-color-switcher.ba6e1521.js.map
